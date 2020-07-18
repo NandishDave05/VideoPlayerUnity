5158373543f76bed438339529f018cea347f1ef0
Nandish Dave <nandishdave5@gmial.com>
2020-07-17 18:03:31 -0700

    Updated commit

--- Assets/Scripts/CanvasController.cs
+++ Assets/Scripts/CanvasController.cs
@@ -6,11 +6,15 @@ public class CanvasController : MonoBehaviour
 {
     public Canvas canvas1;
     
+    public bool action1;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        canvas1.enabled = false;
-      
+        canvas1.enabled = action1;
+       
+
     }
 
     // Update is called once per frame
@@ -19,5 +23,6 @@ public class CanvasController : MonoBehaviour
         
     }
 
+    
  
 }
